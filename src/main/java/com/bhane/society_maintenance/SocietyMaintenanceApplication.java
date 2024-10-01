590bf3e357c9c8378030db8d11127a873b9b2ff1
Kedar Bhanegaonkar <ksbhanegaonkar@gmail.com>
2024-10-01 21:38:15 +0530

    Initial commit

---
+++ src/main/java/com/bhane/society_maintenance/SocietyMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.bhane.society_maintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocietyMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocietyMaintenanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhane/society_maintenance/SocietyMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhane.society_maintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocietyMaintenanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
